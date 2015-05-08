689973288200a8a69e811d1991a3b828eb20944f
sugim48 <sugim48@gmail.com>
2015-05-08 14:34:32 +0900

    aaa

--- talk/listen/listen.py
+++ talk/listen/listen.py
@@ -1,7 +1,7 @@
 import speech_recognition as sr
 
 class Listener:
-	def __init__(self, energy_threshold):
+	def __init__(self, energy_threshold = 300):
 		self.r = sr.Recognizer(language = "ja")
 		self.r.energy_threshold = energy_threshold
 	def listen(self):
