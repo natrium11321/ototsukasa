cd191309c5df219c9492110424b01a7b2f0b2873
gfngfn <bd.gfngfn@gmail.com>
2015-05-08 16:14:23 +0900

    small fix of cooperate.py

--- talk/cooperate.py
+++ talk/cooperate.py
@@ -2,17 +2,19 @@ import listen.listen as listener
 import comp.comprehender as comprehender
 
 def main():
-    print ("  'cooperate.py' is running.\n")
+    print ("  'cooperate.py' is running.")
     lsnr = listener.Listener(600)
     result = lsnr.listen()
     if result != None:
+        print ("  writing output file...")
         cmprhdr = comprehender.Comprehender(result)
         words_dict = cmprhdr.comprehend()
         f = open("test_coop.txt", "w")
         f.write("All: " + ", ".join(words_dict['all']).encode("utf-8") + "\n")
         f.close()
+        print ("  end.")
     else:
-        print "  'None' returned.\n"
+        print ("! no word recognized.")
 
 # ---- Execute ----
 if __name__ == "__main__":
