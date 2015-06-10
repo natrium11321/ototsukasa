8c2cac17e2da68deea5e536851bffebe81c28a40
sugim48 <sugim48@gmail.com>
2015-06-10 18:18:01 +0900

    a

--- talk/hoge.py
+++ talk/hoge.py
@@ -2,6 +2,7 @@ from database import Database
 from drowner import Drowner
 
 database = Database("157.82.7.193")
-drowner = Drowner(database)
-drowner.drown()
-print "unko"
+# drowner = Drowner(database)
+# drowner.drown()
+unlocker = Unlocker(database)
+unlocker.is_locked()
