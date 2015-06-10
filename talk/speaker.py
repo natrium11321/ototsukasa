deef4fef0c736d6c1bff579393e8bf17a4a33605
sugim48 <sugim48@gmail.com>
2015-06-11 03:28:06 +0900

    a

--- talk/speaker.py
+++ talk/speaker.py
@@ -1,13 +1,8 @@
-import pygame
+import subprocess
 
 class Speaker:
 	
 	def __init__(self):
-		pygame.init()
-		pygame.mixer.quit()
-		pygame.mixer.init()
-		self.sound = {}
-		self.sound["test"] = pygame.mixer.Sound("/usr/share/sounds/alsa/Front_Center.wav")
 	
 	def speak(self, name):
-		self.sound[name].play()
+		subprocess.call(["aplay", name])
