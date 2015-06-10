0fd2956a4ccdfd749416f74ed64c2095e7f39067
sugim48 <sugim48@gmail.com>
2015-06-11 02:57:19 +0900

    a

--- talk/poyo.py
+++ talk/poyo.py
@@ -1,15 +1,4 @@
-from datetime import datetime, timedelta
-import json
-import urllib
+from speaker import Speaker
 
-nowtime = datetime.utcnow()
-print nowtime
-updatetime = datetime.utcnow()
-print updatetime
-print updatetime - nowtime < timedelta(minutes = 10)
-
-keyword = raw_input().decode('utf-8')
-url = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8dADJ83IRXa3XMEGie06g3FDxM91enr0&part=id&type=video&q='
-f = urllib.urlopen(url + urllib.quote_plus(keyword.encode('utf-8')))
-data = json.loads(f.read())
-print data
+speaker = Speaker()
+speaker.speak("test")
