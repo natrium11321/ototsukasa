1a36817c2a398d046d12f4cd71f9b7c81219aae7
sugim48 <sugim48@gmail.com>
2015-06-11 01:05:03 +0900

    a

--- talk/player.py
+++ talk/player.py
@@ -5,11 +5,13 @@ import time
 class Player:
 	
 	def __init__(self):
-		thread = threading.Thread(target = target)
+		self.thread = threading.Thread(target = target)
 	
 	def play(self, id):
+		self.thread.start()
 		url = subprocess.check_output(["youtube-dl", "-g", "https://www.youtube.com/watch?v=" + id]).rstrip()
-		p = subprocess.Popen(["omxplayer", "-o", "local", url])
+		self.process = subprocess.Popen(["omxplayer", "-o", "local", url])
 	
 	def target(self):
 		time.sleep(25)
+		self.process.kill()
