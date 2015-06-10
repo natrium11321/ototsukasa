ed893193d0f412b9b41f6e9c35c0647a4e694c99
sugim48 <sugim48@gmail.com>
2015-06-11 03:02:41 +0900

    a

--- talk/speaker.py
+++ talk/speaker.py
@@ -3,6 +3,7 @@ import pygame
 class Speaker:
 	
 	def __init__(self):
+		pygame.init()
 		pygame.mixer.init()
 		self.sound = {}
 		self.sound["test"] = pygame.mixer.Sound("/usr/share/sounds/alsa/Front_Center.wav")
