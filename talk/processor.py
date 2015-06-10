e2240a1308a9bbe25a56c0b98f97f496134b8ad6
gfngfn <bd.gfngfn@gmail.com>
2015-06-10 19:32:28 +0900

    maybe developed processor.py _

--- talk/processor.py
+++ talk/processor.py
@@ -65,7 +65,7 @@ def mode_home():
   else:
     norecogn = 0
     words_dict = comprehender.comprehend(result)
-    print (", ".join(words_dict['all']).encode("utf-8"))
+    print (" ".join(words_dict['all']).encode("utf-8"))
 
     #( 超絶単純な判定によるコマンド認識 )
     if u"終了" in words_dict['all']:
