17ab6dce098ab3473f06327bf3d9be0fea3d1c12
gfngfn <bd.gfngfn@gmail.com>
2015-06-10 17:33:31 +0900

    refined processor.py but it still has bugs around Unicode

--- talk/processor.py
+++ talk/processor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import listener
-import comprehender
+from listener import Listener
+from comprehender import Comprehender
+#from database import Database
+#from drowner import Drowner
 
 #---- enumeration ----
 MODE_UNLOCK = -2
@@ -15,12 +17,14 @@ MODE_EVAL = 4
 #---- global variable ----
 norecogn = 0
 mode = MODE_LOCKED
-lsnr = listener.Listener(600)
-cmprhdr = comprehender.Comprehender()
+listener = Listener(600)
+comprehender = Comprehender()
 
+#( get_input : unit -> (string | None) )
 def get_input():
   print "* waiting input..."
-  return lsnr.listen()
+#  return listener.listen()
+  return raw_input()
 
 def deal_with_no_recognition():
   norecogn += 1
@@ -60,17 +64,17 @@ def mode_home():
     print ("!---No word recognized.")
   else:
     norecogn = 0
-    words_dict = cmprhdr.comprehend(result)
+    words_dict = comprehender.comprehend(result)
     print (", ".join(words_dict['all']).encode("utf-8"))
 
     #( 超絶単純な判定によるコマンド認識 )
-    if "終了" in words_dict['noun']:
+    if u"終了" in words_dict['all']:
       mode = MODE_QUIT
-    elif "検索" in words_dict['noun']:
+    elif u"検索" in words_dict['all']:
       mode = MODE_SEARCH
-    elif "再生" in words_dict['noun']:
+    elif u"再生" in words_dict['all']:
       mode = MODE_PLAY
-    elif "評価" in words_dict['noun']:
+    elif u"評価" in words_dict['all']:
       mode = MODE_EVAL
     else:
       print "!---No command recognized."
@@ -87,6 +91,7 @@ def mode_search():
   else:
     #(should be written)
     #( 検索：Drawnerを呼ぶ )
+    pass
 
   return
 
@@ -95,6 +100,7 @@ def mode_play():
   #(should be written)
   #( 再生：Drawnerを呼ぶ )
 
+  return
 
 def mode_eval():
   print "  [review mode]"
@@ -105,6 +111,7 @@ def mode_eval():
   else:
     #(should be written)
     #( 評価処理：ReviewSenderを呼ぶ )
+    pass
 
   return
 
