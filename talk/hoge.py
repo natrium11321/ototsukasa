dadee1df254ac7f57979cf88a71b6fbab193409e
sugim48 <sugim48@gmail.com>
2015-06-10 16:31:00 +0900

    a

--- talk/hoge.py
+++ talk/hoge.py
@@ -1,7 +1,7 @@
 from database import Database
 from drowner import Drowner
 
-database = Database("157.82.6.126")
+database = Database("157.82.7.193")
 drowner = Drowner(database)
 drowner.drown(raw_input())
 print "unko"
