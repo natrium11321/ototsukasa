9b7ed90450e71192c0f9c9328117463a8676b6ac
sugim48 <sugim48@gmail.com>
2015-06-10 23:43:25 +0900

    a

--- talk/poyo.py
+++ talk/poyo.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+import json
+import urllib
 
 nowtime = datetime.utcnow()
 print nowtime
@@ -6,6 +8,6 @@ updatetime = datetime.utcnow()
 print updatetime
 print updatetime - nowtime < timedelta(minutes = 10)
 
-a = "?&="
-a = a.replace("?", "\\?")
-print a
+url = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8dADJ83IRXa3XMEGie06g3FDxM91enr0&part=id&type=video&q='
+f = urllib.urlopen(url + urllib.quote_plus(keyword.encode('utf-8')))
+print f
