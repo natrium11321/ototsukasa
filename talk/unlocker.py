e42d5bd006ed36c39e2e102a1b9e865be29cfaa6
sugim48 <sugim48@gmail.com>
2015-06-10 18:20:13 +0900

    a

---
+++ talk/unlocker.py
@@ -0,0 +1,19 @@
+from database import Database
+
+class Unlocker:
+	
+	def __init__(self, db):
+		self.db = db
+		return
+	
+	def is_locked(self):
+		row = db.fetch_reservation()
+		time = row["updatetime"]
+		password = row["password"]
+		print time
+		print password
+		return
+	
+	def unlock(self):
+		
+		return
