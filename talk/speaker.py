983df6ae5c540e27d869d642aa219d060bd5ea94
sugim48 <sugim48@gmail.com>
2015-06-11 03:09:50 +0900

    a

--- talk/speaker.py
+++ talk/speaker.py
@@ -4,9 +4,10 @@ class Speaker:
 	
 	def __init__(self):
 		pygame.init()
+		pygame.mixer.quit()
 		pygame.mixer.init()
 		self.sound = {}
-		self.sound["test"] = pygame.mixer.Sound("/usr/share/sounds/alsa/aFront_Center.wav")
+		self.sound["test"] = pygame.mixer.Sound("/usr/share/sounds/alsa/Front_Center.wav")
 	
 	def speak(self, name):
 		self.sound[name].play()
