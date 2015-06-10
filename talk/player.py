8eeb65cccc50ecfd3f6a5f4fdee2456c1f93d7d1
sugim48 <sugim48@gmail.com>
2015-06-10 23:11:06 +0900

    a

--- talk/player.py
+++ talk/player.py
@@ -8,7 +8,7 @@ class Player:
 	def play(self, id):
 		url = subprocess.check_output(["youtube-dl", "-g", "https://www.youtube.com/watch?v=" + id])
 		print url
-		url.replace("?", "\\?")
-		url.replace("&", "\\&")
-		url.replace("=", "\\=")
+		url = url.replace("?", "\\?")
+		url = url.replace("&", "\\&")
+		url = url.replace("=", "\\=")
 		subprocess.call(["omxplayer", "-o", "local", url])
