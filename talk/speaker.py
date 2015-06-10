a826906ae0f5eafe8cf2ffb60d64a0e4e965b397
Taihei Oki <t.oki.natrium@gmail.com>
2015-06-11 04:06:42 +0900

    moved poyo.py into speaker.py

--- talk/speaker.py
+++ talk/speaker.py
@@ -1,9 +1,16 @@
 import subprocess
 
 class Speaker:
-	
+
 	def __init__(self):
 		pass
-		
+
 	def speak(self, name):
 		subprocess.call(["aplay", name])
+
+def main():
+	speaker = Speaker()
+	speaker.speak(raw_input().rstrip())
+
+if __name__ == '__main__':
+	main()
