f78e67afd8ad1983c77d0bef6bd0b7cc3127b7c1
sugim48 <sugim48@gmail.com>
2015-06-10 23:37:22 +0900

    a

--- talk/you_tube_searcher.py
+++ talk/you_tube_searcher.py
@@ -8,7 +8,7 @@ class YouTubeSearcher:
 	
 	def search(self, keyword):
 		url = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyB8dADJ83IRXa3XMEGie06g3FDxM91enr0&part=id&type=video&q='
-		f = urllib.urlopen(url + urllib.quote_plus(keyword))
+		f = urllib.urlopen(url + urllib.quote_plus(keyword.encode('utf-8')))
 		data = json.loads(f.read())
 		if data["pageInfo"]["totalResults"] > 0:
 			return data["items"][0]["id"]["videoId"]
