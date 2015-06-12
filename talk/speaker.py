8c640acac0474488a699d335f5b341be2709c6a6
sugim48 <sugim48@gmail.com>
2015-06-12 15:13:14 +0900

    a

--- talk/speaker.py
+++ talk/speaker.py
@@ -5,6 +5,7 @@ class Speaker:
 	def __init__(self):
 		pass
 	
+	# speed : 50-300
 	def speak(self, text, speed = 100):
 		subprocess.call("/home/pi/aquestalkpi/AquesTalkPi -s " + str(speed) + " " + text + " | aplay", shell = True)
 
