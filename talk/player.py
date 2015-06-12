51428f2bb38f070a2315e435c3013964705ceb10
sugim48 <sugim48@gmail.com>
2015-06-12 19:13:56 +0900

    a

--- talk/player.py
+++ talk/player.py
@@ -8,6 +8,6 @@ class Player:
 	
 	def play(self, id):
 		url = subprocess.check_output(["youtube-dl", "-g", "https://www.youtube.com/watch?v=" + id]).rstrip()
-		self.process = subprocess.Popen(["omxplayer", "-o", "local", "--vol", "-10", url], stdin = subprocess.PIPE)
+		self.process = subprocess.Popen(["omxplayer", "-o", "local", "--vol", "-20", url], stdin = subprocess.PIPE)
 		time.sleep(20)
 		self.process.communicate("q")
