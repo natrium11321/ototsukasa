b1777340191be75ab450b9dfeef03b517070dcfe
Takahiro Sugiura <tksg2333@gmail.com>
2015-06-15 19:14:12 +0900

    [Bug fix] Only one info window is open.

--- web/index.js
+++ web/index.js
@@ -9,69 +9,68 @@ function initialize() {
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 
+  var currentInfoWindow = null;
   $.getJSON("/cgi-bin/get_info.py?sex=M",function(data){
-    var currentInfoWindow = null;
     for (var i = 0; i < data.length ; i++){
       info = data[i];
-      currentInfoWindow = makeMarker(info,currentInfoWindow);
+      makeMarker(info);
     }
   })
 
-};
+  function makeMarker(info) {
 
-function makeMarker(info,prevInfoWindow) {
+    //トイレの状態を表すピクトグラム
+    var empty ="white.png";
+    var occupied = "red.png";
+    var reserved = "yellow.png";
 
-  //トイレの状態を表すピクトグラム
-  var empty ="white.png";
-  var occupied = "red.png";
-  var reserved = "yellow.png";
-
-  var icon;
-  if (info["empty_num"] == 0){
-    icon = "red.png";
-  }else if(info["empty_num"] == 1){
-    icon = "yellow.png";
-  }else{
-    icon = "white.png";
-  }
-
-  var content = '<h1>' + info["name"] + '</h1>'
-
-  for(var i=0;i<info["toilets"].length;i++){
-    if (info["toilets"][i] === "Empty"){
-      content += '<img src="' + empty +'" width="30px" height="30px" alt="空き">';
-    }else if (info["toilets"][i] === "Reserved") {
-      content += '<img src="'+reserved+'" width="30px" height="30px" alt="予約済み">';
-    }else if (info["toilets"][i] === "Occupied") {
-      content += '<img src="'+occupied+'" width="30px" height="30px" alt="使用中">';
+    var icon;
+    if (info["empty_num"] == 0){
+      icon = "red.png";
+    }else if(info["empty_num"] == 1){
+      icon = "yellow.png";
+    }else{
+      icon = "white.png";
     }
-  }
 
-  content += '<p>最新のレビュー:' + info["review_comment"] + '</p>'
+    var content = '<h1>' + info["name"] + '</h1>'
 
-  //空きがあるときは予約ボタン
-  if (info["empty_num"] > 0){
-    content +=   '<p><form action="/cgi-bin/reserve.py" method="POST"><input type="hidden" name="sex" value="M"><input type="hidden" name="pos_id" value=' + info["pos_id"] + '><div class="buttonarea"><input type="submit" value="予約" class="btn btn-default"></div></form></p>'
-  }
-
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(info["lat"],info["lng"]),
-    map: map,
-    icon: icon
-  });
-
-
-  var infowindow = new google.maps.InfoWindow({
-    content: content
-  });
-
-  google.maps.event.addDomListener(marker,'click',function(){
-    if(prevInfoWindow){
-      prevInfoWindow.close();
+    for(var i=0;i<info["toilets"].length;i++){
+      if (info["toilets"][i] === "Empty"){
+        content += '<img src="' + empty +'" width="30px" height="30px" alt="空き">';
+      }else if (info["toilets"][i] === "Reserved") {
+        content += '<img src="'+reserved+'" width="30px" height="30px" alt="予約済み">';
+      }else if (info["toilets"][i] === "Occupied") {
+        content += '<img src="'+occupied+'" width="30px" height="30px" alt="使用中">';
+      }
     }
-    infowindow.open(map,marker);
-  })
-  return infowindow
+
+    content += '<p>最新のレビュー:' + info["review_comment"] + '</p>'
+
+    //空きがあるときは予約ボタン
+    if (info["empty_num"] > 0){
+      content +=   '<p><form action="/cgi-bin/reserve.py" method="POST"><input type="hidden" name="sex" value="M"><input type="hidden" name="pos_id" value=' + info["pos_id"] + '><div class="buttonarea"><input type="submit" value="予約" class="btn btn-default"></div></form></p>'
+    }
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(info["lat"],info["lng"]),
+      map: map,
+      icon: icon
+    });
+
+
+    var infowindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addDomListener(marker,'click',function(){
+      if(currentInfoWindow){
+        currentInfoWindow.close();
+      }
+      infowindow.open(map,marker);
+      currentInfoWindow = infowindow;
+    })
+  };
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
